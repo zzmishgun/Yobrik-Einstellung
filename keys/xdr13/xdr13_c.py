a9602a4a7d395c9e253b6b4b1d17c1f6a71ed08e
zzmishgun <49115254+zzmishgun@users.noreply.github.com>
2019-11-09 20:43:23 +0300

    Revert "Add files via upload"
    This reverts commit 6fe9d672cd2b1da4a5359622cf4c313d2c3bc705.

--- keys/xdr13/xdr13_c.py
+++
@@ -1,9 +0,0 @@
-from keys.xdr13 import dict
-
-def key_xdr13():
-    text = ''
-    message = list(input('ENTER the MESSAGE >>  '))
-    сharacters = len(message)
-    for i in range(сharacters):
-        text += dict.dict_main[message[i]]
-    print('             ',text,sep='')
